53637d02bb1a6ed07b7d6511fca552367798a3f5
Jason Ludu <s01367095@acad.tri-c.edu>
2025-08-02 14:10:25 -0400

    Add files via upload

---
+++ C# Projects/BasicCalculatorProject/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BasicCalcProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double UserNum1;
+            double UserNum2;
+
+            Console.WriteLine("Welcome to the calculator app!");
+            Console.WriteLine("Please type in your first number");
+            UserNum1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please type in your second number");
+            UserNum2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Your first number plus the second number is: ");
+            Console.WriteLine(UserNum1 + UserNum2);
+
+            Console.WriteLine("Your first number minus the second number is: ");
+            Console.WriteLine(UserNum1 - UserNum2);
+
+            Console.WriteLine("Your first number multiplied by the second number is: ");
+            Console.WriteLine(UserNum1 * UserNum2);
+
+            Console.WriteLine("Your first number divided by the second number is: ");
+            Console.WriteLine(UserNum1 / UserNum2);
+
+            Console.WriteLine("Press any key to end the program");
+            Console.ReadKey();
+        }
+    }
+}
