7ddb5551814138af289c775786109f8c7ed6d478
Jason Ludu <s01367095@acad.tri-c.edu>
2025-08-02 14:18:45 -0400

    Add files via upload

---
+++ C# Projects/Pi Calculator Project/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CalcOfPi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Radius;
+
+            Console.WriteLine("Please input the radius of a circle: ");
+            Radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Area: ");
+            Console.WriteLine(Math.PI * Math.Pow(Radius, 2));
+
+            Console.WriteLine("Circumference: ");
+            Console.WriteLine(2 * Math.PI * Radius);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
