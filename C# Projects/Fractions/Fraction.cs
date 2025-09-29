ecd0859a0b9429bf8f58e30c910e88cae7eab8c2
Jason Ludu <s01367095@acad.tri-c.edu>
2025-09-29 17:43:43 -0400

    Add files via upload

---
+++ C# Projects/Fractions/Fraction.cs
@@ -0,0 +1,112 @@
+﻿class Fraction
+{
+    int numerator;
+    int denominator;
+
+    public Fraction(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public void Multiply(int numerator, int denominator)
+    {
+        this.numerator *= numerator;
+        this.denominator *= denominator;
+    }
+
+    public void Multiply(int wholeNumber)
+    {
+        this.numerator *= wholeNumber;
+    }
+
+    public void Multiply(Fraction fraction)
+    {
+        this.numerator *= fraction.numerator;
+        this.denominator *= fraction.denominator;
+    }
+
+    public void Divide(int numerator, int denominator)
+    {
+        this.numerator *= denominator;
+        this.denominator *= numerator;
+    }
+
+    public void Divide(int wholeNumber)
+    {
+        this.denominator *= wholeNumber; 
+    }
+
+    public void Divide(Fraction fraction)
+    {
+        this.numerator *= fraction.denominator;
+        this.denominator *= fraction.numerator;
+    }
+
+    public void Add(int numerator, int denominator)
+    {
+        int newDenominator;
+        int newNumerator;
+        
+        newDenominator = this.denominator * denominator;
+        newNumerator = (this.numerator * denominator) + (numerator * this.denominator);
+
+        this.numerator = newNumerator;
+        this.denominator = newDenominator;
+    }
+
+    public void Add(int wholeNumber)
+    {
+        this.numerator += wholeNumber * this.denominator;
+    }
+
+    public void Add(Fraction fraction)
+    {
+        int newDenominator;
+        int newNumerator;
+        
+        newDenominator = this.denominator * fraction.denominator;
+        newNumerator = (this.numerator * fraction.denominator) + (fraction.numerator * this.denominator);
+        
+        this.numerator = newNumerator;
+        this.denominator = newDenominator;
+    }
+
+    public void Subtract(int numerator, int denominator)
+    {
+        int newDenominator;
+        int newNumerator;
+
+        newDenominator = this.denominator * denominator;
+        newNumerator = (this.numerator * denominator) - (numerator * this.denominator);
+
+        this.numerator = newNumerator;
+        this.denominator = newDenominator;
+    }
+
+    public void Subtract(int wholeNumber)
+    {
+        this.numerator -= wholeNumber * this.denominator;
+    }
+
+    public void Subtract(Fraction fraction)
+    {
+        int newDenominator;
+        int newNumerator;
+
+        newDenominator = this.denominator * fraction.denominator;
+        newNumerator = (this.numerator * fraction.denominator) - (fraction.numerator * this.denominator);
+
+        this.numerator = newNumerator;
+        this.denominator = newNumerator;
+    }
+
+    public string DisplayFraction()
+    {
+        string Fraction;
+
+        Fraction = this.numerator.ToString() + "/" + this.denominator.ToString();
+
+        return Fraction;
+    }
+}
---
+++ C# Projects/Fractions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Program
+{
+    public static void Main()
+    {
+        Fraction f1 = new Fraction(5, 6);
+        Fraction f2 = new Fraction(3, 4);
+
+        f1.Multiply(3, 2);
+        f2.Add(1, 2);
+        f2.Subtract(1);
+        f1.Subtract(f2);
+        f1.Divide(3);
+
+        Console.WriteLine("Fraction 1 is now: " + f1.DisplayFraction());
+        Console.WriteLine("Fraction 2 is now: " + f2.DisplayFraction());
+
+
+        Console.ReadKey();
+    }
+}
