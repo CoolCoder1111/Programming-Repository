4af1c0dd7b6677d41a9a1db1ff74c5fbe9973292
Jason Ludu <s01367095@acad.tri-c.edu>
2025-12-12 16:26:47 -0500

    Add files via upload

---
+++ C# Projects/Hutt&HikeProject/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Hut_Hike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 7 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("Hutt and hike");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("hutt");
+                }
+                else if (i % 7 == 0)
+                { 
+                    Console.WriteLine("hike");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
