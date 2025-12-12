9ef6beef1942b6ee3d1e6ecad235972ed9bff65f
Jason Ludu <s01367095@acad.tri-c.edu>
2025-12-12 12:39:26 -0500

    Add files via upload

---
+++ C# Projects/EmployeeInfo project/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeeinfo
+{
+    internal class Employee
+    {
+        private string name;
+        private string department;
+        private int salary;
+
+        public Employee(string name, string department, int salary)
+        {
+            this.name = name;
+            this.department = department;
+            this.salary = salary;
+        }
+
+        public void Raise()
+        {
+            salary = salary + 1000;
+        }
+
+        public void Raise(int raisePassed)
+        {
+            salary = salary + raisePassed;
+        }
+
+        public void changeDepartment(string depart)
+        {
+            department = depart;
+
+        }
+
+        public string getInfo()
+        {
+            return name + " works at " + department + " and has a salary of " + salary + " dollars";
+
+        }
+
+
+
+    }
+}
---
+++ C# Projects/EmployeeInfo project/Program.cs
@@ -0,0 +1,40 @@
+﻿using Employeeinfo;
+using System;
+using System.ComponentModel;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Create three employee objects
+        //You can completely make up the names, departments, and salaries of each employee
+        Employee employee1 = new Employee("Tom" , "Construction", 100);
+        Employee employee2 = new Employee("Finn", "Helpdesk", 200);
+        Employee employee3 = new Employee("Abigail", "Packaging", 300);
+
+        //Display the 'info' for each employee
+
+        Console.WriteLine(employee1.getInfo());
+        Console.WriteLine(employee2.getInfo());
+        Console.WriteLine(employee3.getInfo());
+
+        //Give the first employee a generic raise
+
+        employee1.Raise(1000);
+
+        //Give change the second employee's department to... something different
+
+        employee2.changeDepartment("Demolition");
+
+        //Give the third employee a raise of 5000
+
+        employee3.Raise(5000);
+
+        //Display the 'info' for each employee again
+        Console.WriteLine();
+        Console.WriteLine(employee1.getInfo());
+        Console.WriteLine(employee2.getInfo());
+        Console.WriteLine(employee3.getInfo());
+
+    }
+}
