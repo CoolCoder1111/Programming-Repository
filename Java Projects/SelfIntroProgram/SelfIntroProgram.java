67bec720a80a2eaf65f8bdb55a8a2e242694fa5d
Jason Ludu <s01367095@acad.tri-c.edu>
2025-12-12 13:12:34 -0500

    Add files via upload

---
+++ Java Projects/SelfIntroProgram/SelfIntroProgram.java
@@ -0,0 +1,12 @@
+package com.mycompany.selfintroprogram;
+public class SelfIntroProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Bentley");
+        System.out.println("My major is Dog Training");
+        System.out.println("My class goal is... wait why am I here again?");
+        System.out.println("My goals for my degree is to be the nicest and cutest little dog in the world!");
+        System.out.println("I want to be better than everyone!");
+        System.out.println("This is my picture! https://imgur.com/a/bentley-QRKIT1H");
+    }
+}
