4e444b627e9b320f23e53590961727ceef918edc
Jason Ludu <s01367095@acad.tri-c.edu>
2025-12-12 16:36:33 -0500

    Add files via upload

---
+++ C# Projects/Hypotenuse Project/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace hypotenusecalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Altitude;
+            int Base;
+
+            Console.WriteLine("Welcome to the Hypotenuse Calculator!");
+            Console.WriteLine("Please input a value for the Altitude of the right triangle");
+            Altitude= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please input the value for the base of the right triangle");
+            Base = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The hypotenuse would be...");
+            Console.WriteLine(Math.Sqrt(Math.Pow(Altitude, 2) + Math.Pow(Base, 2)));
+            Console.WriteLine("Please press enter to end the application");
+            Console.ReadKey();
+        }
+    }
+}
