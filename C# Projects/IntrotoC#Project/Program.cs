39fc65181883df956bef3ecd9ef7f8abf97a73c2
Jason Ludu <s01367095@acad.tri-c.edu>
2025-12-12 16:39:40 -0500

    Add files via upload

---
+++ C# Projects/IntrotoC#Project/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jason Ludu");
+            Console.WriteLine("The experience I have with programming is I have used Java and Python and just starting to use C#");
+            Console.WriteLine("I became interested in computers because I find them facinating to work with and programming is interesting because I get to experience what it is like to code");
+            Console.WriteLine("My expectations for this class is to learn about how to code and eventually learn to code with other languages");
+            Console.WriteLine("My goals after graduating from Tri-C is to go to a four year college to get a bachelors degree in Computer Science");
+            Console.WriteLine("One big dream goal of mine is to work in I.T as a user support/hardware specialist");
+            Console.WriteLine("Press any key to end this program...");
+            Console.ReadKey();
+        } 
+    }
+}
